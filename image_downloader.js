614f142a6f199ec2d40343923a231ffc01cd93b8
Milan Pandurov <milanpandurov@pm.me>
2018-11-22 10:59:38 +0100

    Only download images that are missing

--- image_downloader.js
+++ image_downloader.js
@@ -7,8 +7,13 @@ const https = require('https');
 const fs = require('fs');
 
 const BASE_URL="https://www.amazon.com/drive/v1/"
+const ARCHIVE_NAME="archive.zip"
+const SAVE_FILE="downloaded_images.json"
+const GOOD_ERROR="no new images in album"
 
-function downloadSharedGalery(galeryId){
+var existingImages={}
+
+function downloadSharedGalery(galeryId, cb){
 	var shareInfoUrl=buildUrl(url.resolve(BASE_URL, "shares/"+galeryId),
 		{
 			queryParams:{
@@ -65,28 +70,66 @@ function downloadSharedGalery(galeryId){
 		nodeIds=[]
 
 		response.data.data.forEach(element => {
-			nodeIds.push(element.ownerId+":"+element.id)
+			newId=element.ownerId+":"+element.id
+			if (!(newId in existingImages)){
+				existingImages[newId]=true
+				nodeIds.push(newId)
+			}
 		})
+
+		if (nodeIds.length==0){
+			throw new Error(GOOD_ERROR)
+		}
+
 		compressImagesRequest.nodeIds=nodeIds
 
 		return axios.post(batchLinkUrl, compressImagesRequest);
 	})
 	.then(response => {
-		var file = fs.createWriteStream("archive.zip");
-
+		var file = fs.createWriteStream(ARCHIVE_NAME);
 		var request = https.get(response.data.links.content, function(response) {
+
+			if (response.statusCode !== 200) {
+				return cb('Response status was ' + response.statusCode);
+			}
+
 			response.pipe(file);
-		});
+
+			file.on('finish', function(){
+				file.close(cb);
+			});
+		}).on('error', function(err){
+			fs.unlink(ARCHIVE_NAME)
+			cb(err)
+		})
 	})
 	.catch(error => {
-		console.log(error)
+		cb(error)
 	})
+}
+
+function saveFile(){
+	var json = JSON.stringify(existingImages)
+	fs.writeFileSync(SAVE_FILE, json, 'utf8');
+}
+
+function loadFile(){
+	var content = fs.readFileSync(SAVE_FILE)
+	existingImages=JSON.parse(content)
 }
 
 if (process.argv.length<3){
 	console.log("Not enough arguments");
 }else{
-	downloadSharedGalery(process.argv[2])
+	loadFile()
+
+	downloadSharedGalery(process.argv[2], error =>{
+		if (error && error.message!=GOOD_ERROR){
+			console.log(error)
+			process.exit(1)
+		}
+		saveFile()
+	})
 }
 
 
